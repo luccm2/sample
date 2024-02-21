83c22cac7713a1673eb435ea111c0c325b52c010
Lucas Merquior <lucas.merquior@gmail.com>
2024-02-21 14:21:05 -0300

    Create sample files

---
+++ sample.go
@@ -0,0 +1,11 @@
+package sample
+
+// Example represents ...
+type Example struct {
+	Name string `json:"name"`
+}
+
+// NewExample creates a new instance of Example.
+func NewExample(name string) *Example {
+	return &Example{Name: name}
+}
